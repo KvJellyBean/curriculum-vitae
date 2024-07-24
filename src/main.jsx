3deb67c38ad4ce3368b195e7f4da4bf42a6d35bd
KvJellyBean <kevinnatan91@gmail.com>
2024-07-24 18:48:59 +0700

    feat: add routing for the website

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Hero from "./components/Hero.jsx";
+import Skills from "./components/Skills.jsx";
+import Portfolio from "./components/Portfolio.jsx";
+import Education from "./components/Education.jsx";
+import EducationDetail from "./components/EducationDetail.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ErrorPage } from "./components/ErrorPage.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Hero />,
+        index: true,
+      },
+      {
+        path: "/skills",
+        element: <Skills />,
+      },
+      {
+        path: "/education",
+        element: <Education />,
+      },
+      {
+        path: "/education/:id",
+        element: <EducationDetail />,
+      },
+      {
+        path: "/portfolio",
+        element: <Portfolio />,
+      },
+    ],
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
