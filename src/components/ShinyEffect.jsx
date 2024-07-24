492517c58a353a4d1cc2f08dc34e66afd24188d1
KvJellyBean <kevinnatan91@gmail.com>
2024-07-24 19:24:53 +0700

    feat: add shiny effect background component

---
+++ src/components/ShinyEffect.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ShinyEffect = () => {
+  return (
+    <div className="shiny-effect absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[90dvw] h-[95dvh] -z-10 overflow-hidden"></div>
+  );
+};
+
+export default ShinyEffect;
